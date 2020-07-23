86e171ba2df23b6a1c6671910f813999c8ff9dcf
Flavio Donzé <flavio.donze@scodi.ch>
2020-07-23 11:51:15 +0200

    prepare for war build

--- oauth-rest/oauth-authorization-server/src/main/java/com/baeldung/auth/AuthorizationServerApp.java
+++ oauth-rest/oauth-authorization-server/src/main/java/com/baeldung/auth/AuthorizationServerApp.java
@@ -6,8 +6,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 
@@ -15,12 +17,17 @@ import com.baeldung.auth.config.KeycloakServerProperties;
 
 @SpringBootApplication(exclude = LiquibaseAutoConfiguration.class)
 @EnableConfigurationProperties({ KeycloakServerProperties.class })
-public class AuthorizationServerApp {
+public class AuthorizationServerApp extends SpringBootServletInitializer {
 
 	private static final Logger LOG = LoggerFactory.getLogger(AuthorizationServerApp.class);
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(AuthorizationServerApp.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(AuthorizationServerApp.class);
 	}
 
 	@Bean
